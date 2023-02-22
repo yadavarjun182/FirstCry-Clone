ce157d9de36630d6040bfa010acf0ea62a6554e9
Rahul Deb <rahulbnc463@gmail.com>
2023-02-22 23:10:44 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import "./App.css";
 import FooterPage from "./pages/FooterPage";
 
@@ -7,19 +6,6 @@ function App() {
     <div>
       <FooterPage />
     </div>
-
-
-import './App.css';
-import AdminRegister from "./Components/Admin/AdminRegister";
-import { ChakraProvider } from "@chakra-ui/react";
-// import AdminLogin from "./Components/Admin/AdminLogin";
-
-import { AccoutDetails } from './pages/AccountDetails/AccoutDetails';
-
-function App() {
-  return (
-    <div> </div>
-
   );
 }
 
