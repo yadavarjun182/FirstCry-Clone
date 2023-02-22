fb35d1af9f1f8bb2a3bea9e88cc5446c61b4e760
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-22 17:53:55 +0530

    fw19_0493 day -1

--- frontend/src/components/Navbar2.jsx
+++ frontend/src/components/Navbar2.jsx
@@ -1,14 +1,32 @@
 import {
-    Box,
+    Box,UnorderedList,ListItem
   } from "@chakra-ui/react";
 
   export default function Navbar2() {
   
     return (
       <>
-        <Box bg={"#ffd91c"}>
-          
-        </Box>
+            <Box bg={"#ffd91c"} >
+                <Box w={'95%'} m='auto'>
+        <UnorderedList display={'flex'} fontWeight={500} listStyleType='none' justifyContent={'space-between'} w={'95%'} m='auto'>
+  <ListItem>ALL CATEGORIES</ListItem>
+  <ListItem>BOY FASHION</ListItem>
+  <ListItem>GIRL FASHION</ListItem>
+                    <ListItem>FOOTWEAR</ListItem>
+                    <ListItem>TOYS</ListItem>
+                    <ListItem>DIAPERING</ListItem>
+                    <ListItem>GEAR</ListItem>
+                    <ListItem>FEEDING</ListItem>
+                    <ListItem>BATH</ListItem>
+                    <ListItem>NURSERY</ListItem>
+                    <ListItem>MOMS</ListItem>
+                    <ListItem>HEALTH</ListItem>
+                    <ListItem>BOUTIQUES</ListItem>
+                    <ListItem>CLUB</ListItem>
+                    <ListItem>carter's</ListItem>
+</UnorderedList>
+                </Box>
+                </Box>
       </>
     );
   }
