d14d48e8343d9d545e7d7a647336a2ed97dc8ba1
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-23 12:52:59 +0530

    fw19_0493_day_2

--- frontend/src/components/Navbar2.jsx
+++ frontend/src/components/Navbar2.jsx
@@ -1,6 +1,29 @@
-import { Box, UnorderedList,Text } from "@chakra-ui/react";
-import {Link} from "react-router-dom"
-export default function Navbar2() {
+import { Box, UnorderedList,Text, useDisclosure } from "@chakra-ui/react";
+//import {Text} from "react-router-dom";
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+  } from '@chakra-ui/react'
+import React from "react";
+export default function Navbar2()
+{
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [placement, setPlacement] = React.useState('top')
+  
+    const handleClick=() =>
+    {
+        onOpen()
+    }
+    
+    const handleClick2=() =>
+    {
+        onOpen()
+    }
   return (
     <>
       <Box bg={"#ffd91c"} px={4}>
@@ -16,21 +39,43 @@ export default function Navbar2() {
                       m="auto"
                       alignContent={'center'}
           >
-            <Link to='#' _hover={{bg:'white'}}>ALL CATEGORIES</Link>
-            <Link to='#' _hover={{bg:'white'}}>BOY FASHION</Link>
-            <Link to='#' _hover={{bg:'white'}}>GIRL FASHION</Link>
-            <Link to='#' _hover={{bg:'white'}}>FOOTWEAR</Link>
-            <Link to='#' _hover={{bg:'white'}}>TOYS</Link>
-            <Link to='#' _hover={{bg:'white'}}>DIAPERING</Link>
-            <Link to='#' _hover={{bg:'white'}}>GEAR</Link>
-            <Link to='#' _hover={{bg:'white'}}>FEEDING</Link>
-            <Link to='#' _hover={{bg:'white'}}>BATH</Link>
-            <Link to='#' _hover={{bg:'white'}}>NURSERY</Link>
-            <Link to='#' _hover={{bg:'white'}}>MOMS</Link>
-            <Link to='#' _hover={{bg:'white'}}>HEALTH</Link>
-            <Link to='#' _hover={{bg:'white'}}>BOUTIQUES</Link>
-            <Link to='#' _hover={{bg:'white',h:'full'}} textAlign='center'>CLUB</Link>
-            <Link to='#'><Text bg="skyblue" color={'white'} p={4} textAlign='center' fontSize={21}>carter's</Text></Link>
+            <Text  _hover={{bg:'white'}}>ALL CATEGORIES</Text>
+                      <Text _hover={{bg: 'white'}} onClick={handleClick}>BOY FASHION</Text>
+                      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+          <DrawerBody>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+                      <Text _hover={{bg: 'white'}} onClick={handleClick2}>GIRL FASHION</Text>
+                      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+          <DrawerBody>
+            <p>Some contents...jjdj</p>
+            <p>Some contents...dfhdf</p>
+            <p>Some contentsdfhdh...</p>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+            <Text  _hover={{bg:'white'}}>FOOTWEAR</Text>
+            <Text  _hover={{bg:'white'}}>TOYS</Text>
+            <Text  _hover={{bg:'white'}}>DIAPERING</Text>
+            <Text  _hover={{bg:'white'}}>GEAR</Text>
+            <Text  _hover={{bg:'white'}}>FEEDING</Text>
+            <Text  _hover={{bg:'white'}}>BATH</Text>
+            <Text  _hover={{bg:'white'}}>NURSERY</Text>
+            <Text  _hover={{bg:'white'}}>MOMS</Text>
+            <Text  _hover={{bg:'white'}}>HEALTH</Text>
+            <Text  _hover={{bg:'white'}}>BOUTIQUES</Text>
+            <Text  _hover={{bg:'white',h:'full'}} textAlign='center'>CLUB</Text>
+            <Text bg="skyblue" color={'white'} p={4} textAlign='center' fontSize={21}>carter's</Text>
           </UnorderedList>
         </Box>
       </Box>
