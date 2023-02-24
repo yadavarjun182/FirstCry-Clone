f09e3499947012e5b29f49c825d57e5649a6ed4a
unknown <vikeshvhinge@gmail.com>
2023-02-24 22:16:41 +0530

    new-try-1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,6 @@
-
-import "./App.css";
-import Home from "./pages/home";
-
 import './App.css';
 import Navbar from './components/Navbar';
 import FooterPage from "./pages/FooterPage";
-
 import { Cart } from './pages/Cart';
 import {Register} from './pages/Authantication/signup';
 import {Login} from './pages/Authantication/Login'
@@ -16,17 +11,12 @@ function App() {
   
   return (
 
-    <div>
-      <Home />
-
-
       <div>
       <Navbar/>
        <Register/>
        <Login/> 
        <Cart/>
       <FooterPage />
-
     </div>
   );
 }
