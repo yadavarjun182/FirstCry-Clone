f66ddbe00d7754c860798eb116d105c5a4dc9806
Arjun Yadav <yadavarjun182@gmail.com>
2023-02-25 15:32:10 +0530

    products backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
-//mongodb+srv://arjun:arjun@cluster0.3wtn3cm.mongodb.net/firstcry?retryWrites=true&w=majority
-const connection = mongoose.connect("mongodb+srv://manish-nishad:manish-nishad@cluster0.4yljajf.mongodb.net/firstcry?retryWrites=true&w=majority")
+
+const connection = mongoose.connect("mongodb+srv://arjun:arjun@cluster0.3wtn3cm.mongodb.net/firstcry?retryWrites=true&w=majority");
+// const connection = mongoose.connect("mongodb+srv://manish-nishad:manish-nishad@cluster0.4yljajf.mongodb.net/firstcry?retryWrites=true&w=majority")
 
 
 module.exports = { connection }
