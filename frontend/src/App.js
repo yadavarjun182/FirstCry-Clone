de2324cf567942a016a05fff3a796192d7ecdc4b
unknown <vikeshvhinge@gmail.com>
2023-02-26 13:26:34 +0530

    --day-final--

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,10 @@ import { Cart } from "./pages/Cart";
 import { Register } from "./pages/Authantication/signup";
 import { Login } from "./pages/Authantication/Login";
 import ProductsPage from "./pages/ProductsA/ProductsPage";
-
-import AdminRegister from "./Components/Admin/AdminRegister";
-import AdminLogin from "./Components/Admin/AdminLogin";
-
-
-import SideMenu from "./Components/Admin/Components/SideMenu/index";
-import PageContent from "./Components/Admin/Components/PageContent/index";
-import AppHeader from './Components/Admin/Components/AppHeader';
-import AdminDashboard from './Components/Admin/AdminDashboard';
+import SideMenu from "./components/Admin/Components/SideMenu/index";
+import PageContent from "./components/Admin/Components/PageContent/index";
+import AppHeader from './components/Admin/Components/AppHeader';
+import AdminDashboard from './components/Admin/AdminDashboard';
 import { AllRouts } from "./V_AllRouts/Allrouts";
 import { ToastContainer } from "react-toastify";
 
--- frontend/src/V_AllRouts/Allrouts.jsx
+++ frontend/src/V_AllRouts/Allrouts.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import  AdminLogin  from "../Components/Admin/AdminLogin";
-import  AdminRegister from "../Components/Admin/AdminRegister";
+import  AdminLogin  from "../components/Admin/AdminLogin";
+import  AdminRegister from "../components/Admin/AdminRegister";
 import 'react-toastify/dist/ReactToastify.css';
 import { AccoutDetails } from "../pages/AccountDetails/AccoutDetails";
 import ProductsPage from "../pages/ProductsA/ProductsPage";
-import AdminDashboard from "../Components/Admin/AdminDashboard";
-import Customers from "../Components/Admin/Pages/Customers/index";
-import Dashboard from "../Components/Admin/Pages/Dashbaord/index";
-import Inventory from "../Components/Admin/Pages/Inventory/index";
-import Orders from "../Components/Admin/Pages/Orders/index";
+import AdminDashboard from "../components/Admin/AdminDashboard";
+import Customers from "../components/Admin/Components/PageContent/index";
+import Dashboard from "../components/Admin/Pages/Dashbaord/index";
+import Inventory from "../components/Admin/Pages/Inventory/index";
+import Orders from "../components/Admin/Pages/Orders/index";
 
 
 export const AllRouts = () => {
