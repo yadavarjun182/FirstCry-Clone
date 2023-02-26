e732a94c70f41a436970998f2cce3c0f3db140cf
Arjun Yadav <yadavarjun182@gmail.com>
2023-02-26 20:29:09 +0530

    final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
-
+import Navbar from "./Components/Navbar";
 import FooterPage from "./pages/FooterPage";
-
-
 import { AllRouts } from "./V_AllRouts/Allrouts";
-
-
 function App() {
   return (
     <div>
-   
+      <Navbar />
       <AllRouts />
+      <FooterPage />
     </div>
 
   );
