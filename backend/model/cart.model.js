482a27e1144bf3cb0a7155716c0eab36fbb5c9b8
Arjun Yadav <yadavarjun182@gmail.com>
2023-02-27 18:33:17 +0530

    few changes

--- backend/model/cart.model.js
+++ backend/model/cart.model.js
@@ -1,15 +1,14 @@
-const mongoose = require ("mongoose")
+const mongoose = require("mongoose")
 
 const cartSchema = mongoose.Schema({
-    title:String,
-    thumbnail:String,
-    mrp:Number,
-    Cart_quantity:Number,
-    mrp:Number,
-    discount:Number,
-    user:String
+    title: String,
+    thumbnail: String,
+    Cart_quantity: Number,
+    mrp: Number,
+    discount: Number,
+    user: String
 })
 
-const CartModel = mongoose.model('cart',cartSchema)
+const CartModel = mongoose.model('cart', cartSchema)
 
-module.exports= {CartModel}
+module.exports = { CartModel }
--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,28 +1,28 @@
-const express = require ('express')
+const express = require('express')
 const cartRouter = express.Router()
-const {CartModel} = require('../model/cart.model.js')
+const { CartModel } = require('../model/cart.model.js')
 
 
-cartRouter.get('/',async(req,res)=>{
-    try{
+cartRouter.get('/', async (req, res) => {
+    try {
         let quary = req.body.user
-        const data = await CartModel.find({user:quary})
-        if(data.length <= 0){
+        const data = await CartModel.find({ user: quary })
+        if (data.length <= 0) {
             res.send('cart is empty !!!')
-        }else{
+        } else {
             let total = 0
             let price = 0
             let discount = 0
-            for(let i=0; i<data.length; i++){
-                total = total+data[i].mrp;
-                price = price+((data[i].mrp+((data[i].mrp*data[i].discount)/100)))
-                discount = discount+((data[i].mrp*data[i].discount)/100)
+            for (let i = 0; i < data.length; i++) {
+                total = total + data[i].mrp;
+                price = price + ((data[i].mrp + ((data[i].mrp * data[i].discount) / 100)))
+                discount = discount + ((data[i].mrp * data[i].discount) / 100)
             }
-            res.send({'data':data,'total':total,"price":price,"discount":discount})
+            res.send({ 'data': data, 'total': total, "price": price, "discount": discount })
         }
-      
-    }catch(err){
-        res.send({'msg':err})
+
+    } catch (err) {
+        res.send({ 'msg': err })
     }
 })
 
@@ -31,17 +31,18 @@ cartRouter.post('/addtocart',async(req,res)=>{
 
     try{
         const product = req.body
-        console.log('daaaaaaaaaataaaaaaa',req.body)
-        console.log('product',product.title)
-        let data = await CartModel.find({title:product.title})
-       // console.log('data',data)
+        //console.log('daaaaaaaaaataaaaaaa',req.body)
+        //console.log('product',product)
+        //let data = await CartModel.find({title:product.title})
+        let data = await CartModel.find({$and:[{title:product.title},{user:product.user}]})
+        console.log('data',data)
         if(data.length > 0){
             res.send({"msg":'Product present in Cart !'})
         }
         else{
             const Cart_product = new CartModel(product)
         await Cart_product.save()
-        res.send("Product is added to Cart")
+        res.send({"msg":'Product Added in Cart !'})
         }
     }catch(err){
         console.log({'err':err})
@@ -51,36 +52,36 @@ cartRouter.post('/addtocart',async(req,res)=>{
 
 
 
-cartRouter.patch('/cartupdate/:id',async(req,res)=>{
-    try{
+cartRouter.patch('/cartupdate/:id', async (req, res) => {
+    try {
         let id = req.params.id
-        let product = await CartModel.find({_id:req.params.id})
+        let product = await CartModel.find({ _id: req.params.id })
         let quantity = product[0].quantity + 1;
-        if(product.length > 0){
-            await CartModel.findByIdAndUpdate({_id:req.params.id},{quantity})
+        if (product.length > 0) {
+            await CartModel.findByIdAndUpdate({ _id: req.params.id }, { quantity })
         }
         res.send(`product with id ${id} is updated`)
-    }catch(err){
-        res.send({'err':err.message})
-    }
-}) 
-
-
-
-cartRouter.delete('/cartdelete/:id',async(req,res)=>{
-   
-    try{
-        let id = req.params.id
-        await CartModel.findByIdAndDelete({_id:id})
-        let quary = req.body.user
-        console.log(quary)
-        const data = await CartModel.find({user:quary})
-        res.send(data)
-    }
-    catch(err){
-        console.log(err.message)
-        res.send({'err':err.message})
+    } catch (err) {
+        res.send({ 'err': err.message })
     }
 })
 
-module.exports = {cartRouter}
+
+
+cartRouter.delete('/cartdelete/:id', async (req, res) => {
+
+    try {
+        let id = req.params.id
+        await CartModel.findByIdAndDelete({ _id: id })
+        let quary = req.body.user
+        console.log(quary)
+        const data = await CartModel.find({ user: quary })
+        res.send(data)
+    }
+    catch (err) {
+        console.log(err.message)
+        res.send({ 'err': err.message })
+    }
+})
+
+module.exports = { cartRouter }
